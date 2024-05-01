4ead1ae9c273792880dd9a31699a296a9a909c61
Murilo Sopi <dev.murilosopi@gmail.com>
2024-05-01 13:09:56 -0300

    propriedade img para ContainerSecao

--- src/components/ContainerSecao.vue
+++ src/components/ContainerSecao.vue
@@ -1,17 +1,35 @@
 <template>
-    <section class="container">
-        <slot></slot>
-    </section>
+  <section class="container" :style="style">
+    <div class="conteudo">
+      <slot></slot>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    img: String,
+  },
+  computed: {
+    style() {
+      return this.img
+        ? {
+            backgroundImage: `url(${this.img})`,
+            backgroundSize: 'cover',
+            backgroundAttachment: 'fixed',
+          }
+        : false;
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .container {
-        padding: 3rem;
-    }
+.container {
+  padding: 3rem;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+}
 </style>
