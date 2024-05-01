d265f4302e569a95a084aef63768a6ce7ebb43b2
Murilo Sopi <dev.murilosopi@gmail.com>
2024-05-01 13:08:10 -0300

    cria componente TextoDestaque

---
+++ src/components/TextoDestaque.vue
@@ -0,0 +1,26 @@
+<template>
+  <p class="texto-destaque" :class="'-' + variacao">
+    <slot></slot>
+  </p>
+</template>
+
+<script>
+export default {
+    props: {
+        variacao: {
+            type: String,
+            default: ''
+        }
+    }
+};
+</script>
+
+<style scoped>
+.texto-destaque {
+  font-size: 2.5em;
+}
+
+.texto-destaque.-branco {
+  color: var(--branco);
+}
+</style>
