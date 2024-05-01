d16021f65dcba85501dcacde0b436478ae6a568e
Murilo Sopi <dev.murilosopi@gmail.com>
2024-05-01 20:12:49 -0300

    cria componentes CampoTexto e ContainerForm

---
+++ src/components/common/CampoTexto.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="campo-texto">
+    <label v-if="nome" :for="id">{{ nome }}</label>
+    <input
+      type="text"
+      :id="id"
+      :name="name"
+      ref="inputCpfCnpj"
+      v-model="valor"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id", "name", "nome", "value"],
+  data() {
+    return {
+      valor: this.value,
+    };
+  },
+  watch: {
+    valor() {
+      this.$emit("input", this.valor);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+.campo-texto, .campo-texto > * {
+  width: 100%;
+}
+
+.campo-texto label {
+  font-weight: 500;
+  font-style: italic;
+}
+.campo-texto input,
+.campo-texto label {
+  display: block;
+}
+
+.campo-texto input {
+  border-radius: 10px;
+  background-color: var(--secundario);
+  padding: .25rem 1.25rem;
+  border: none;
+  outline: none;
+  transition: .3s;
+}
+
+.campo-texto input:focus {
+  box-shadow: 0px 1px 3px 1px var(--destaque-pastel);
+  outline: 1px solid var(--destaque);
+}
+</style>
---
+++ src/components/layout/ContainerForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <form class="container-form" @submit.prevent="(e) => $emit('submit', e)">
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container-form {
+  padding: 1.5rem 2rem 2rem 2rem;
+  border: 1px solid var(--secundario);
+  border-radius: 1.2rem;
+}
+</style>
