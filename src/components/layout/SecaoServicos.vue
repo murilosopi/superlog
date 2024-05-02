4afe3db527104b4766ea19f7ef078fdefde917c5
Murilo Sopi <dev.murilosopi@gmail.com>
2024-05-02 08:06:23 -0300

    cria SecaoServicos

---
+++ src/components/layout/SecaoServicos.vue
@@ -0,0 +1,37 @@
+<template>
+  <ContainerSecao
+    id="rastreamento"
+    :img="require('@/assets/img/banners/2.png')"
+  >
+    <TituloPrincipal tag="h2" :tamanho="2" variacao="branco">
+      <i class="fa-solid fa-map-location-dot"></i>
+      Serviços
+    </TituloPrincipal>
+
+    <p>
+      Na <strong>Super Log</strong>, entendemos a importância de armazenar e
+      transportar suas mercadorias com eficiência e segurança. Somos
+      especialistas em oferecer soluções abrangentes para todas as suas
+      necessidades logísticas, desde armazenamento até entrega rápida. Aqui está
+      um vislumbre dos serviços que oferecemos:
+    </p>
+  </ContainerSecao>
+</template>
+
+<script>
+import ContainerSecao from "@/components/layout/ContainerSecao.vue";
+import TituloPrincipal from "@/components/common/TituloPrincipal.vue";
+
+export default {
+  components: {
+    ContainerSecao,
+    TituloPrincipal,
+  },
+};
+</script>
+
+<style scoped>
+#rastreamento {
+  color: var(--branco);
+}
+</style>
--- src/views/PaginaInicial.vue
+++ src/views/PaginaInicial.vue
@@ -3,6 +3,7 @@
     <CabecalhoPrincipal :flutuante="true" />
     <SliderSobre />
     <SecaoRastreamento />
+    <SecaoServicos />
   </main>
 </template>
 
@@ -10,12 +11,14 @@
 import CabecalhoPrincipal from "@/components/layout/CabecalhoPrincipal.vue";
 import SliderSobre from "@/components/layout/SliderSobre.vue";
 import SecaoRastreamento from "@/components/layout/SecaoRastreamento.vue";
+import SecaoServicos from "@/components/layout/SecaoServicos.vue";
 
 export default {
   components: {
     CabecalhoPrincipal,
     SliderSobre,
     SecaoRastreamento,
+    SecaoServicos,
   },
 };
 </script>
