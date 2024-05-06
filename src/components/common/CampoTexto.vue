bd3c5c58450252e1da5b01dc99b4398961f55a96
Murilo Sopi <dev.murilosopi@gmail.com>
2024-05-05 23:54:19 -0300

    adiciona variação de textarea ao CampoTexto

--- src/components/common/CampoTexto.vue
+++ src/components/common/CampoTexto.vue
@@ -2,18 +2,35 @@
   <div class="campo-texto">
     <label v-if="nome" :for="id">{{ nome }}</label>
     <input
+      v-if="type != 'textarea'"
       type="text"
       :id="id"
       :name="name"
-      ref="inputCpfCnpj"
       v-model="valor"
     />
+    <textarea
+      v-else
+      :id="id"
+      :name="name"
+      v-model="valor"
+      :rows="rows"
+    ></textarea>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["id", "name", "nome", "value"],
+  props: {
+    id: String,
+    name: String,
+    nome: String,
+    value: String,
+    rows: Number,
+    type: {
+      type: String,
+      default: "text",
+    },
+  },
   data() {
     return {
       valor: this.value,
@@ -28,31 +45,38 @@ export default {
 </script>
 
 <style scoped>
-
-.campo-texto, .campo-texto > * {
-  width: 100%;
+.campo-texto,
+.campo-texto > * {
+  min-width: 100%;
+  max-width: 100%;
 }
 
 .campo-texto label {
   font-weight: 500;
   font-style: italic;
+  margin-bottom: 0.2rem;
 }
 .campo-texto input,
+.campo-texto textarea,
 .campo-texto label {
   display: block;
 }
 
-.campo-texto input {
+.campo-texto input,
+.campo-texto textarea {
   border-radius: 10px;
   background-color: var(--secundario);
-  padding: .25rem 1.25rem;
+  padding: 0.25rem 1.25rem;
   border: none;
   outline: none;
-  transition: .3s;
+  transition: 0.1s linear;
+  outline: 1px solid darkgray;
 }
 
-.campo-texto input:focus {
-  box-shadow: 0px 1px 3px 1px var(--destaque-pastel);
+.campo-texto input:focus,
+.campo-texto textarea:focus {
+  box-shadow: 0px 0px 3px 1px var(--destaque);
+  border: none;
   outline: 1px solid var(--destaque);
 }
 </style>
